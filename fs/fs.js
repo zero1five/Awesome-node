a45e8fe7db8f29ece67035c24be943b5e163f49f
zero1five <2642585002@qq.com>
2018-05-06 23:34:04 +0800

    :blush:

--- fs/fs.js
+++ fs/fs.js
@@ -1,1 +1,27 @@
 const fs = require('fs')
+
+/**
+ * fs.open(path, flags, [mode], callback)
+ *  path： 要打开的文件路径
+ *  flags： 打开文件的方式 读/写
+ *  设置文件的模式： 读/写/执行
+ *  callback： 回调
+ *      err： 文件打开失败的错误保存在err里, 如果成功则为null
+ *      fd： 被打开文件的标识
+ */
+
+fs.open('1.txt', 'r', (err, fd) => {
+    if (err) {
+       console.log('文件打开失败') 
+    } else {
+        console.log(fd)
+    }
+})
+
+fs.open('1.txt', 'r', (err, fd) => {
+    if (err) {
+       console.log('文件打开失败') 
+    } else {
+        console.log(fd)
+    }
+})
